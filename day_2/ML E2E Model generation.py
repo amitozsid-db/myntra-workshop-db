4e631e29e528cabfa752eaf7ec7a2b091c9c93a8
amitoz.sidhu@databricks.com <amitoz.sidhu@databricks.com>
2023-04-01 09:25:57 +0000

    added startup code for running dbdemos setup

--- day_2/ML E2E Model generation.py
+++ day_2/ML E2E Model generation.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
+# MAGIC %pip install dbdemos
 
+# COMMAND ----------
+
+import dbdemos
+dbdemos.list_demos()
+
+# COMMAND ----------
+
+dbdemos.install('mlops-end2end')
